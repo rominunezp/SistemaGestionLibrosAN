d2e3a0050c8b292a3dd88081784907253813249f
Anhely <anhelyromina64@gmail.com>
2025-05-03 20:24:33 -0500

    Primer commit con clase Principal

---
+++ SistemaGestionLibrosANP/src/librosapp/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librosapp;
+
+/**
+ *
+ * @author Sistema
+ */
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al Sistema de Gestión de Libros");
+    }
+}
